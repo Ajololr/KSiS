b95c2ec138f1cbd67f77fe9b83d0fe380c490965
Ilya Androsov <ilya_androsov@icloud.com>
2020-05-12 19:20:34 +0300

    feat: add file controller element

--- lab-2/src/Client/ChatWindow/ChatWindow.java
+++ lab-2/src/Client/ChatWindow/ChatWindow.java
@@ -1,21 +1,62 @@
 package Client.ChatWindow;
 
 import Client.ClientSocket.*;
+import Client.FileStorageManager.UniqueFile;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.File;
 import java.util.LinkedList;
+import java.util.Random;
 
 
 public class ChatWindow extends Frame implements WindowListener {
     public LinkedList<ChatMessages> chatsMessagesList = new LinkedList<>();
     public Button sendButton;
+    public Button addFileButton;
     public List chatsList;
     public TextField msgField;
     public TextArea chatArea;
     private ClientSocket client;
+    private Container newChatArea;
     private int currentChatIndex;
 
+    private class FileMessage {
+        public Container controlPane;
+        public Label fileNameLabel;
+    }
+
+    private class ChatMessage {
+        public Container controlPane;
+        public Button downloadButton;
+        public Label mes;
+        public String message;
+        public FileMessage[] filesArray;
+    }
+
+    private Panel createFileControlElement(File file) {
+        Panel controlPane;
+        Button deleteButton;
+        Label fileNameLabel;
+        controlPane = new Panel();
+        int rgbColor = (int)(Math.random() * 0xFFFFFF);
+        controlPane.setBackground(new Color(rgbColor));
+        controlPane.setLayout(new FlowLayout());
+        deleteButton = new Button("Delete file");
+        deleteButton.addActionListener(actionEvent -> {
+            remove(controlPane);
+            updateLayout();
+        });
+        fileNameLabel = new Label(file.getName());
+        fileNameLabel.setForeground(new Color(rgbColor ^ 0xFFFFFF));
+        controlPane.add(fileNameLabel);
+        controlPane.add(deleteButton);
+        controlPane.setVisible(true);
+        return controlPane;
+    }
 
     private class ChatMessages {
         public String chatName;
@@ -29,6 +70,11 @@ public class ChatWindow extends Frame implements WindowListener {
         }
     }
 
+    private void updateLayout() {
+        this.setVisible(false);
+        this.setVisible(true);
+    }
+
     public ChatWindow(ClientSocket client) {
         addWindowListener(this);
         this.client = client;
@@ -36,6 +82,7 @@ public class ChatWindow extends Frame implements WindowListener {
         setLayout(new FlowLayout(FlowLayout.LEFT));
 
         sendButton = new Button("Send");
+        addFileButton = new Button("Add file");
 
         chatsList = new List(15, false);
         chatsList.add("Global chat");
@@ -66,6 +113,21 @@ public class ChatWindow extends Frame implements WindowListener {
             }
         });
 
+        addFileButton.addActionListener(actionEvent -> {
+            FileDialog fileDialog = new FileDialog((Frame) null);
+            fileDialog.setVisible(true);
+            String filename = fileDialog.getFile();
+            String directory = fileDialog.getDirectory();
+            if (filename == null)
+                System.out.println("You cancelled the choice");
+            else {
+                System.out.println("You chose " + directory + filename);
+                File file = new File(directory + filename);
+                add(createFileControlElement(file));
+                updateLayout();
+            }
+        });
+
         msgField.addActionListener(actionEvent -> {
             if (!msgField.getText().trim().equals("")) {
                 client.send(msgField.getText().trim(), currentChatIndex);
@@ -77,8 +139,9 @@ public class ChatWindow extends Frame implements WindowListener {
         add(chatsList);
         add(msgField);
         add(sendButton);
+        add(addFileButton);
 
-        this.setSize(500,310);
+        this.setSize(500,400);
         this.setVisible(true);
         this.setTitle(client.nickname);
     }
